c213b3608bf51475ead6f5603b48914950e3a7ba
Chani17 <cucute8878@naver.com>
2023-07-24 21:17:25 +0900

    Feat: Update code

--- src/main/java/inu/thebite/umul/repository/EatingHabitRepository.java
+++ src/main/java/inu/thebite/umul/repository/EatingHabitRepository.java
@@ -16,14 +16,16 @@ public interface EatingHabitRepository extends JpaRepository<EatingHabit, Long> 
 
     boolean existsByDate(String date);
 
-    @Query(value = "SELECT e.total_count FROM eating_habit e WHERE e.child_id = (SELECT c.child_id FROM children c WHERE c.name = :childName AND c.phone_number = : phoneNumber) AND e.date = :date ORDER BY e.created_at DESC LIMIT 1 ", nativeQuery = true)
-    List<Integer> findTotalCountTopByDateAndChildrenNameOrderByCreatedAtDesc(String date, String childName, String phoneNumber);
+//    @Query(value = "SELECT e.total_count FROM eating_habit e WHERE e.child_id = (SELECT c.child_id FROM children c WHERE c.name = :childName AND c.phone_number = : phoneNumber) AND e.date = :date ORDER BY e.created_at DESC LIMIT 1 ", nativeQuery = true)
+//    List<Integer> findTotalCountTopByDateAndChildrenNameOrderByCreatedAtDesc(String date, String childName, String phoneNumber);
 
+    @Query(value = "SELECT e.total_count FROM eating_habit e WHERE e.child_id = (SELECT c.child_id FROM children c WHERE c.name = :childName AND c.phone_number = :phoneNumber) AND e.date = :date ORDER BY e.created_at DESC LIMIT 1", nativeQuery = true)
+    List<Integer> findTotalCountTopByDateAndChildrenNameOrderByCreatedAtDesc(@Param("date") String date, @Param("childName") String childName, @Param("phoneNumber") String phoneNumber);
     @Query(value = "SELECT e.total_time FROM eating_habit e WHERE e.child_id = (SELECT c.child_id FROM children c WHERE c.name = :childName AND c.phone_number = : phoneNumber) AND e.date = :date ORDER BY e.created_at DESC LIMIT 1 ", nativeQuery = true)
-    List<Integer> findTotalTimeTopByDateAndChildrenNameOrderByCreatedAtDesc(String date, String childName, String phoneNumber);
+    List<Integer> findTotalTimeTopByDateAndChildrenNameOrderByCreatedAtDesc(@Param("date") String date, @Param("childName") String childName, @Param("phoneNumber")String phoneNumber);
 
     @Query(value = "SELECT e.bite_count_with_mouth FROM eating_habit e WHERE e.child_id = (SELECT c.child_id FROM children c WHERE c.name = :childName AND c.phone_number = : phoneNumber) AND e.date = :date ORDER BY e.created_at DESC LIMIT 1", nativeQuery = true)
-    List<Integer> findBitCountWithMouthTopByDateAndChildrenNameOrderByCreatedAtDesc(String date, String childName, String phoneNumber);
+    List<Integer> findBitCountWithMouthTopByDateAndChildrenNameOrderByCreatedAtDesc(@Param("date")String date, @Param("childName")String childName, @Param("phoneNumber")String phoneNumber);
 
 //    @Query(value = "SELECT count_per_success FROM eating_habit WHERE name = :childName AND date = :date ORDER BY created_at DESC LIMIT 1", nativeQuery = true)
 //    List<Integer> findCountPerSuccessByDateAndChildrenNameOrderByCreatedAtDesc(String date, String childName);
